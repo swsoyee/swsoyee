33002e5786749db881a641b111dc0081cec6e48f
swsoyee <swsoyee@gmail.com>
2020-07-27 12:08:16 +0900

    add: citation of tcc-gui paper

--- src/Fetch.R
+++ src/Fetch.R
@@ -1,5 +1,6 @@
 library(jsonlite)
 library(data.table)
+library(rvest)
 
 # 1. ==== Fetch stats of PSNINE enhance script ====
 psnine_enhance_script_json <- read_json("https://greasyfork.org/zh-CN/scripts/375985-psn%E4%B8%AD%E6%96%87%E7%BD%91%E5%8A%9F%E8%83%BD%E5%A2%9E%E5%BC%BA/stats.json")
@@ -18,8 +19,17 @@ covid_2019_live_stats <- data.table(
 
 pv <- sum(covid_2019_live_stats$pv)
 
+# 3. ==== Fetch TCC-GUI paper citations
+tcc_gui <- "https://bmcresnotes.biomedcentral.com/articles/10.1186/s13104-019-4179-2/metrics"
+tcc_gui_html <- read_html(tcc_gui)
+tcc_gui_metrics <- tcc_gui_html %>% html_nodes(".c-article-metrics__access-citation") %>% html_text()
+
+citations <- strsplit(x = gsub(pattern = "  ", replacement = "", x = tcc_gui_metrics), split = "\n")[[3]][2]
+
+# ==== Create JSON ====
 data <- list(
   psnine_enhance_install = sum(unlist(psnine_enhance_data$installs)),
+  citations = as.numeric(citations),
   covid_2019_live_pv = pv
 )
 
